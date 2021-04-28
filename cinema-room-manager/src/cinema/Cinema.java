6b22693738aa05a71a5f7886d8da407b620ff7aa
robee <muhammad.robee@gmail.com>
2021-04-29 00:24:39 +0700

    cinema-room-manager stage 4

--- cinema-room-manager/src/cinema/Cinema.java
+++ cinema-room-manager/src/cinema/Cinema.java
@@ -14,8 +14,26 @@ public class Cinema {
         int columns = Integer.parseInt(scanner.nextLine());
 
         RoomManager roomManager = new RoomManager(rows, columns);
-        roomManager.printSeats(0, 0);
 
+        int chosen = printMenu(scanner);
+        while (chosen != 0) {
+            switch (chosen) {
+                case 1:
+                    roomManager.printSeats();
+                    break;
+                case 2:
+                    buyTicket(roomManager, scanner);
+                    break;
+                default:
+                    System.out.printf("Please choose either 1, 2, or 0.");
+                    break;
+            }
+            chosen = printMenu(scanner);
+        }
+
+    }
+
+    public static void buyTicket(RoomManager roomManager, Scanner scanner) {
         System.out.println("Enter a row number:");
         int row = Integer.parseInt(scanner.nextLine());
 
@@ -23,7 +41,15 @@ public class Cinema {
         int column = Integer.parseInt(scanner.nextLine());
 
         System.out.printf("Ticket price: $%d\n\n", roomManager.getPrice(row));
-        roomManager.printSeats(row, column);
+        roomManager.addChosenSeats(row, column);
     }
-    
+
+    public static int printMenu(Scanner scanner) {
+        System.out.println("1. Show the seats");
+        System.out.println("2. Buy a ticket");
+        System.out.println("0. Exit");
+
+        return Integer.parseInt(scanner.nextLine());
+    }
+
 }
--- cinema-room-manager/src/cinema/RoomManager.java
+++ cinema-room-manager/src/cinema/RoomManager.java
@@ -3,10 +3,25 @@ package cinema;
 public class RoomManager {
     int rows;
     int columns;
+    boolean[][] chosenSeats;
 
     public RoomManager(int rows, int columns) {
         this.rows = rows;
         this.columns = columns;
+        initSeats();
+    }
+
+    private void initSeats() {
+        this.chosenSeats = new boolean[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                this.chosenSeats[i][j] = false;
+            }
+        }
+    }
+
+    public void addChosenSeats(int row, int column) {
+        this.chosenSeats[row - 1][column - 1] = true;
     }
 
     public int getPrice(int row) {
@@ -26,13 +41,13 @@ public class RoomManager {
         return price;
     }
 
-    public void printSeats(int row, int column) {
+    public void printSeats() {
         printHeader();
 
         for (int i = 1; i <= rows; i++) {
             StringBuilder seats = new StringBuilder();
             for (int j = 1; j <= columns; j++) {
-                if (i == row && j == column) {
+                if (this.chosenSeats[i-1][j-1]) {
                     seats.append(" B");
                 } else {
                     seats.append(" S");
