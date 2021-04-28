3955d100220018ff8cc1a054e744220ea33ac079
robee <muhammad.robee@gmail.com>
2021-04-28 23:26:52 +0700

    cinema-room-manager stage 2

--- cinema-room-manager/src/cinema/Cinema.java
+++ cinema-room-manager/src/cinema/Cinema.java
@@ -1,12 +1,29 @@
 package cinema;
 
+import java.util.Scanner;
+
 public class Cinema {
 
     public static void main(String[] args) {
-        System.out.println("Cinema:");
-        System.out.println("  1 2 3 4 5 6 7 8");
-        for (int i = 1; i < 8; i++) {
-            System.out.println(i + " S S S S S S S S");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows:");
+        int rows = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Enter the number of seats in each row:");
+        int columns = Integer.parseInt(scanner.nextLine());
+
+        int price;
+        int seats = rows * columns;
+        if (seats <= 60) {
+            price = 10 * seats;
+        } else {
+            int firstHalf = rows / 2;
+            price = firstHalf * columns * 10;
+            price += (rows - firstHalf) * columns * 8;
         }
+
+        System.out.println("Total income:");
+        System.out.println("$" + price);
     }
 }
